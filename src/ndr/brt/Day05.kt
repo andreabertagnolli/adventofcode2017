87b770bc086c1061be29978eb8170940081cfbd7
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-12-20 09:11:31 +0100

    Day 05 completed

--- src/ndr/brt/Day05.kt
+++ src/ndr/brt/Day05.kt
@@ -6,13 +6,30 @@ class Day05 {
         var steps = 0
 
         while (index >= 0 && index < list.size) {
-            //println(list.map { it.toString() }.joinToString(","))
             val stepTo = list[index]
             list.set(index, stepTo + 1)
             index += stepTo
             steps++
         }
-        //println(list.map { it.toString() }.joinToString(","))
+
+        return steps
+    }
+
+    fun stepsToExitPart2(list: IntArray): Int {
+        var index = 0
+        var steps = 0
+
+        while (index >= 0 && index < list.size) {
+            val stepTo = list[index]
+            var futureValue = when {
+                stepTo >= 3 -> stepTo -1
+                else -> stepTo +1
+            }
+
+            list.set(index, futureValue)
+            index += stepTo
+            steps++
+        }
 
         return steps
     }
---
+++ test/ndr/brt/Day05Test.kt
@@ -0,0 +1,34 @@
+package ndr.brt
+
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+
+class Day05Test {
+
+    val day = Day05()
+
+    @Test
+    internal fun exit_from_list_part1() {
+        assertEquals(5, day.stepsToExitFrom(intArrayOf(0, 3, 0, 1, -3)));
+    }
+
+    @Test
+    internal fun exit_from_list_part2() {
+        assertEquals(10, day.stepsToExitPart2(intArrayOf(0, 3, 0, 1, -3)));
+    }
+
+    @Test
+    internal fun valid_passphrases_part1() {
+        val validPassphrases = Files.readAllLines(Paths.get("resources/day05")).map{ it.toInt() }
+
+        println(day.stepsToExitFrom(validPassphrases.toIntArray()))
+    }
+    @Test
+    internal fun valid_passphrases_part2() {
+        val validPassphrases = Files.readAllLines(Paths.get("resources/day05")).map{ it.toInt() }
+
+        println(day.stepsToExitPart2(validPassphrases.toIntArray()))
+    }
+}

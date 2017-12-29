4d18f752c5801c168a0788881c67d8de76d84629
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-12-29 17:18:16 +0100

    Solve day 09 part 2

--- src/ndr/brt/Day09.kt
+++ src/ndr/brt/Day09.kt
@@ -1,15 +1,17 @@
 package ndr.brt
 
 class Day09 {
-    fun score(input: String): Int {
+    fun score(input: String): Output {
         val closedGroups = mutableListOf<Int>()
 
         var level = 0
         var garbage = false
         var previous = 'x'
+        var garbageSize = 0
 
         input.forEach { it ->
             if (previous != '!') {
+                if (garbage && it != '>' && it != '!') garbageSize++
                 when (it) {
                     '{' -> if (!garbage) level++
                     '}' -> if (!garbage) closedGroups.add(level--)
@@ -24,7 +26,9 @@ class Day09 {
             }
         }
 
-        return closedGroups.sum()
+        return Output(closedGroups.sum(), garbageSize)
     }
+
+    class Output(val score:Int, val garbage: Int)
 
 }
--- test/ndr/brt/Day09Test.kt
+++ test/ndr/brt/Day09Test.kt
@@ -10,53 +10,65 @@ class Day09Test {
 
     @Test
     internal fun with_one_block_returns_one() {
-        assertEquals(1, day.score("{}"))
+        assertEquals(1, day.score("{}").score)
     }
 
     @Test
     internal fun nested_group_count_double() {
-        assertEquals(3, day.score("{{}}"))
+        assertEquals(3, day.score("{{}}").score)
     }
 
     @Test
     internal fun double_nested_group() {
-        assertEquals(6, day.score("{{{}}}"))
+        assertEquals(6, day.score("{{{}}}").score)
     }
 
     @Test
     internal fun two_nested() {
-        assertEquals(5, day.score("{{},{}}"))
+        assertEquals(5, day.score("{{},{}}").score)
     }
 
     @Test
     internal fun various_group() {
-        assertEquals(16, day.score("{{{},{},{{}}}}"))
+        assertEquals(16, day.score("{{{},{},{{}}}}").score)
     }
 
     @Test
     internal fun simple_garbage() {
-        assertEquals(1, day.score("{<a>,<a>,<a>,<a>}"))
+        assertEquals(1, day.score("{<a>,<a>,<a>,<a>}").score)
     }
 
     @Test
     internal fun nested_garbage() {
-        assertEquals(9, day.score("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+        assertEquals(9, day.score("{{<ab>},{<ab>},{<ab>},{<ab>}}").score)
     }
 
     @Test
     internal fun nested_negate_garbage() {
-        assertEquals(9, day.score("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
+        assertEquals(9, day.score("{{<!!>},{<!!>},{<!!>},{<!!>}}").score)
     }
 
     @Test
     internal fun negate_garbage_closing() {
-        assertEquals(3, day.score("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
+        assertEquals(3, day.score("{{<a!>},{<a!>},{<a!>},{<ab>}}").score)
     }
 
     @Test
-    internal fun solution() {
+    internal fun part1_solution() {
         val lines = Files.readAllLines(Paths.get("resources/day09"))
 
-        assertEquals(13154, day.score(lines.get(0)));
+        assertEquals(13154, day.score(lines.get(0)).score);
+    }
+
+    @Test
+    internal fun garbage_size() {
+        assertEquals(10, day.score("<{o\"i!a,<{i<a>").garbage)
+    }
+
+    @Test
+    internal fun part2_solution() {
+        val lines = Files.readAllLines(Paths.get("resources/day09"))
+
+        assertEquals(6369, day.score(lines.get(0)).garbage);
     }
 }
